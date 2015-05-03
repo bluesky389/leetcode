4db5cbd7b9b498f6bff246697a957d287b4da576
Zhezhe Chen <chenzhezhe.osu@gmail.com>
2015-05-02 18:06:46 -0700

    test

--- src/test.java
+++ src/test.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Zhezhe Chen on 5/2/2015.
  */
@@ -5,5 +8,16 @@ public class test {
   public static void main(String args[])
   {
     System.out.println("Hello World");
+    method();
+  }
+
+  public static void method() {
+    System.out.println("test method");
+    List<Integer> integers = new ArrayList<Integer>();
+    for (int i = 0; i < 10; i++) {
+      integers.add(i);
+    }
+
+    System.out.print(integers);
   }
 }
